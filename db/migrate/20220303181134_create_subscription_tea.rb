ca989cbf93378fef888c5e443d8a5c4622b3c4f0
Billy Frey <bfrey16@gmail.com>
2022-03-03 13:50:01 -0700

    add database migrations

---
+++ db/migrate/20220303181134_create_subscription_tea.rb
@@ -0,0 +1,10 @@
+class CreateSubscriptionTea < ActiveRecord::Migration[6.1]
+  def change
+    create_table :subscription_teas do |t|
+      t.references :tea, null: false, foreign_key: true
+      t.references :subscription, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
